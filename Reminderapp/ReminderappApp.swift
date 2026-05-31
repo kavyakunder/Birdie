eb673788f01ae91f4970e2b3dba825cf9ede0201
kavyakunder10 <kavya@compose.co.in>
2026-05-31 19:14:41 +0530

    Initial Commit

---
+++ Reminderapp/ReminderappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderappApp.swift
+//  Reminderapp
+//
+//  Created by Kavya Kunder on 31/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReminderappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
